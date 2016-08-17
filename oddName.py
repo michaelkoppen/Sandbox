f34649bb35e0ddee645d6318d0baeef68738d62e
michaelkoppen <mkoppen98@hotmail.com>
2016-08-18 09:58:19 +1000

    Add name printing program (Better version).

--- oddName.py
+++ oddName.py
@@ -1,5 +1,16 @@
 """
 Michael
 """
-user_name = "Michael"
-print(user_name)
+user_name = input("Enter your name: ")
+while user_name == "":
+    user_name = input("Make sure field is not blank: ")
+count = 1
+for char in list(user_name):
+    if count % 2 != 0:
+        print(char)
+        count += 1
+    else:
+        count +=1
+
+
+
